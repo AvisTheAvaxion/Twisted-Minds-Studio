cacb802ddaa9e20bbf19676e8bad45c73d580f33
Ben <benjoehedges@gmail.com>
2024-02-27 09:25:02 -0500

    Finished Enemy State Machine

--- Hotel of Horrors/Assets/Scripts/EnemyScripts/EnemyStates.cs
+++ Hotel of Horrors/Assets/Scripts/EnemyScripts/EnemyStates.cs
@@ -9,7 +9,13 @@ public class EnemyStates : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] Transform currentRoom;
     [SerializeField] float roomRadius = 5;
+    [SerializeField] float aggroRadius = 9;
+    [SerializeField] float targetDistFromPlayer = 7;
+
+    bool canMove;
     GameObject playerObject;
+    Vector2 randomDir;
+    Vector3 targetPos;
 
     public enum States
     {
@@ -23,6 +29,7 @@ public class EnemyStates : MonoBehaviour
 
     private void Update()
     {
+        print(currentState.ToString());
         switch (currentState)
         {
             case States.idle:
@@ -42,8 +49,19 @@ public class EnemyStates : MonoBehaviour
         return Vector3.Distance(this.transform.position, playerObject.transform.position);
     }
 
+    void GetPosBetweenPlayer()
+    {
+        targetPos = Vector3.MoveTowards(transform.position, playerObject.transform.position, moveSpeed * Time.deltaTime); // calculate distance to move).normalized;
+    }
+
+    void SetRandomDir()
+    {
+        randomDir =  Random.insideUnitCircle; //Get a random position around a unit circle.
+    }
+
     void Idle()
     {
+        //starts patrolling when the player is in the room
         if(GetDistanceToPlayer() < 20)
         {
             currentState = States.patrolling;
@@ -53,22 +71,55 @@ public class EnemyStates : MonoBehaviour
     void Patrol()
     {
         //wander aimlessly around the room
-        Vector2 rng = Random.insideUnitCircle; //Get a random position around a unit circle.
-        var dir = new Vector3(rng.x, 1, rng.y);
+        var dir = new Vector3(randomDir.x, 1, randomDir.y);
         dir.Normalize(); //Ensure magnitude is 1 so we just have a direction
         dir *= roomRadius; //Multiply by the radius to determine how far.
-        dir += currentRoom.position; //This makes the new position relative to the player.
+        dir += transform.position; //This makes the new position relative to the enemy that is moving.
 
+        if (Vector3.Distance(this.transform.position, dir) > 1)
+        {
+            print("moving");
+            var step = moveSpeed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, dir, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            print("chaning destination");
+            SetRandomDir();
+        }
+
+        if(GetDistanceToPlayer() < aggroRadius){
+            currentState = States.fighting;
+        }
     }
 
     void Fight()
     {
-        if(GetDistanceToPlayer() > 7)
+        if (GetDistanceToPlayer() > targetDistFromPlayer)
         {
-            var step = moveSpeed * Time.deltaTime; // calculate distance to move
-            transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, step);
+
+
+            if (canMove)
+            {
+                GetPosBetweenPlayer();
+                transform.position = targetPos;
+                print("moving in attack " + targetPos);
+            }
+            else
+            {
+                StartCoroutine(WaitBeforeMoving());
+            }
+
         }
+        else canMove = false;
         shooterClass.Attack();
     }
 
+
+    IEnumerator WaitBeforeMoving()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(Random.Range(.5f, 2f));
+        canMove = true;
+    }
 }

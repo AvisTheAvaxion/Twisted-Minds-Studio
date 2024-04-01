0f6db813ca0a5bf48ee175234af685c121999094
Waysidechimp <67596048+Waysidechimp@users.noreply.github.com>
2024-04-01 19:13:43 -0400

    Enemy Spawner
    I made it on the test floor since the rooms are more complex. It is under room 8. Valid spawn points are determined by the poly collider. Read comments and descriptions for more understanding.

---
+++ Hotel of Horrors/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField, Description("Monsters will spawn until the queue is depleted")] List<GameObject> enemyQueue;
+    [SerializeField, Description("The amount of monsters that will spawn every wave.")] int spawnPerWave;
+    PolygonCollider2D spawnArea;
+    Queue<GameObject> queue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        queue = new Queue<GameObject>(enemyQueue);
+        spawnArea = GetComponent<PolygonCollider2D>();
+        SpawnWave();
+    }
+
+    void SpawnWave()
+    {
+        Bounds spawnBound = spawnArea.bounds;
+        //Find a random position for every enemy spawn in the wave
+        for (int i = 0; i < spawnPerWave; i++)
+        {
+            Instantiate(queue.Dequeue(), GetRandomPosition(spawnBound), Quaternion.identity);
+        }
+    }
+
+    Vector2 GetRandomPosition(Bounds bound)
+    {
+        //Loop that gets random positions until a position in the Spawnbounds is found
+        while (true)
+        {
+            Vector2 randomSpawn = new Vector2(Random.Range(bound.min.x, bound.max.x), Random.Range(bound.min.y, bound.max.y));
+            //End loop if valid pos
+            if (spawnArea.OverlapPoint(randomSpawn))
+            {
+                return randomSpawn;
+            }
+            //Continue loop if non-valid pos
+            else
+            {
+                continue;
+            }
+
+        }
+    }
+}

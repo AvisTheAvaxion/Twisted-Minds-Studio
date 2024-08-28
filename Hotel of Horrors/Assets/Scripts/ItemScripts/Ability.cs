0b9d0e393a2282912a36ae569781f06b38de708f
Luke Gamage <58281368+T5impact@users.noreply.github.com>
2024-08-29 03:09:43 +0900

    Started on Ability class

--- Hotel of Horrors/Assets/Scripts/ItemScripts/Ability.cs
+++ Hotel of Horrors/Assets/Scripts/ItemScripts/Ability.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Ability
 {
     AbilityInfo info;
+
+    public float cooldown { get; private set; }
+    public float duration { get; private set; }
+    public float damage { get; private set; }
+    public float size { get; private set; }
+    public float range { get; private set; }
+
+    public int numberOfProjectiles { get; private set; }
+    public float deflectionResistance { get; private set; }
+    public int maxTargets { get; private set; }
+    public bool goThroughWalls { get; private set; }
 
     public AbilityInfo GetInfo()
     {
--- Hotel of Horrors/Assets/Scripts/ItemScripts/AbilityInfo.cs
+++ Hotel of Horrors/Assets/Scripts/ItemScripts/AbilityInfo.cs
@@ -6,7 +6,25 @@ using UnityEngine;
 
 public class AbilityInfo : UseableInfo
 {
+    [SerializeField] int unlockEECost = 200;
+    [SerializeField] int unlockFloor = 1;
     [SerializeField] GameObject playerAbility;
+
+    [Header("Ability Settings (Not all Settings apply to every Ability")]
+    [SerializeField] float cooldown;
+    [SerializeField] float duration;
+    [SerializeField] float damage;
+    [SerializeField] float size;
+    [SerializeField] float range;
+
+    [Header("Projectile Settings (Not all may apply")]
+    [SerializeField] int numberOfProjectiles;
+    [SerializeField] float deflectionResistance;
+    [SerializeField] [Range(0, 1)] float chanceToInflictEffect;
+    [SerializeField] EffectInfo[] effectsToInflict;
+    [SerializeField] int maxTargets = 1;
+    [SerializeField] bool goThroughWalls = false;
+
     public override void Use()
     {
         Debug.Log($"Abilities {GetName()} Used");
@@ -14,5 +32,50 @@ public class AbilityInfo : UseableInfo
     public GameObject GetPlayerAbility()
     {
         return playerAbility;
+    }
+
+    public float GetCooldown()
+    {
+        return cooldown;
+    }
+    public float GetDuration()
+    {
+        return duration;
+    }
+    public float GetDamage()
+    {
+        return damage;
+    }
+    public float GetSize()
+    {
+        return size;
+    }
+    public float GetRange()
+    {
+        return range;
+    }
+    public float GetNumOfProjectiles()
+    {
+        return numberOfProjectiles;
+    }
+    public float GetDeflectionResistance()
+    {
+        return deflectionResistance;
+    }
+    public float GetChanceToInflicEffects()
+    {
+        return chanceToInflictEffect;
+    }
+    public EffectInfo[] GetEffectsToInflict()
+    {
+        return effectsToInflict;
+    }
+    public int GetMaxTargets()
+    {
+        return maxTargets;
+    }
+    public bool CanGoThroughWalls()
+    {
+        return goThroughWalls;
     }
 }

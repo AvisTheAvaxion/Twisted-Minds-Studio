a5b4a6ba00590a50de2d21d455ae2c0ceca8c38f
Luke Gamage <58281368+T5impact@users.noreply.github.com>
2024-10-14 21:07:36 -0400

    Changes and additions
    Fixed elevator door visuals
    Reorganized main menu
    Added some mind room UI

---
+++ Hotel of Horrors/Assets/Scripts/ButtonTextVisuals.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ButtonTextVisuals : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] TextMeshProUGUI textHandler;
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color mouseHoverColor = Color.gray;
+    [SerializeField] Color mouseClickColor = Color.black;
+
+    bool mouseOver;
+
+    private void Awake()
+    {
+        if (textHandler == null) textHandler = GetComponent<TextMeshProUGUI>();
+        textHandler.color = defaultColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        textHandler.color = mouseClickColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOver = true;
+        textHandler.color = mouseHoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseOver = false;
+        textHandler.color = defaultColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if(mouseOver)
+        {
+            textHandler.color = mouseHoverColor;
+        }
+        else
+        {
+            textHandler.color = defaultColor;
+        }
+    }
+}
